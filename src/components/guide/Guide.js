b5747f6f98c9d53a8b72023bd584b77b305ee761
oforduhh <oforduhh@gmail.com>
2022-07-02 13:13:50 +0100

    fixed the link

--- src/components/guide/Guide.js
+++ src/components/guide/Guide.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import styles from "./guide.module.scss";
 import gift from "../../assets/Gift.svg";
 import { IoIosArrowForward } from "react-icons/io";
